9d9df7dd46215bbe3da38fea8df0196b4dd6f3cc
gomgomgit <thechocohead@gmail.com>
2020-09-03 21:57:23 +0700

    test port

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Cafe gua
                 </div>
 
                 <div class="links">
